27bea5c9436b6c793bced3aa72c300e39419dbc1
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-01-18 09:25:42 -0600

    edited

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const paragraph = document.querySelector('span.mess-with-me')
-paragraph.style.fontSize = "40px"
-paragraph.addEventListener('click', function(){
-paragraph.style.color = "orange"
+const span = document.querySelector('span.mess-with-me')
+span.style.fontSize = "40px"
+span.addEventListener('click', ()=>{
+span.style.color = "orange"
 })
-const para2 = document.querySelector('p.mess-with-me')
-para2.style.backgroundColor = "green"
+const paragraph = document.querySelector('p.mess-with-me')
+paragraph.style.backgroundColor = "green"
 
 const dino1 = document.querySelector('#triceratops')
 dino1.style.width = "324px"
-dino1.addEventListener('click', function(){
+dino1.addEventListener('click', ()=>{
     dino1.style.border = "10px solid red"
 })
 const dino2 = document.querySelector('#hide-me-area')
 dino2.style.display = "none"
+//dino2.hidden = true
 const dino3 = document.querySelector('#biggify')
 dino3.addEventListener('mouseover', ()=>{
     dino3.style.width = "200px"
@@ -21,7 +22,7 @@ dino3.addEventListener('mouseout', ()=>{
     dino3.style.width = "162px"
 })
 const dino4 = document.querySelector('#feathers')
-dino4.addEventListener('click', function(){
+dino4.addEventListener('click', ()=>{
     dino4.style.opacity = "50%"
 })
 const behindButton = document.querySelector('#row')
